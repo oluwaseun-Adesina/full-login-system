44ca615f29183742e260c92a6d243127d9b430c9
Oluwaseun Adesina <oluwaseunadesina8@gmail.com>
2022-09-21 12:01:33 +0100

    Signup and Login route backend

--- api/User.js
+++ api/User.js
@@ -1,50 +1,52 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
+const bcrypt = require('bcrypt');
 
 //signup
 router.post('/signup', (req, res) => {
     let { name, email, password, dateofbirth } = req.body;
+    console.log(req.body);
     name = name.trim();
     email = email.trim();
     password = password.trim();
     dateofbirth = dateofbirth.trim();
     if (!name || !email || !password || !dateofbirth) {
-        return res.status(400).json({ 
+        return res.status(400).json({
             status: 'error',
-            message: 'Please enter all fields' 
+            message: 'Please enter all fields'
         });
     } else if (!/^[a-zA-Z ]+$/.test(name)) {
-        return res.status(400).json({ 
+        return res.status(400).json({
             status: 'error',
-            message: 'Please enter a valid name' 
+            message: 'Please enter a valid name'
         });
     } else if (!/^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/.test(email)) {
-         res.json({
+        res.json({
             status: 'error',
             message: 'Please enter a valid email'
         });
     }
-        
-    else if (password.length < 8) { 
-        return res.status(400).json({ 
+
+    else if (password.length < 8) {
+        return res.status(400).json({
             status: 'error',
-            message: 'Password must be at least 6 characters' 
+            message: 'Password must be at least 6 characters'
         });
     } else if (!new Date(dateofbirth).getTime()) {
-        return res.status(400).json({ 
+        return res.status(400).json({
             status: 'error',
-            message: 'Please enter a valid date of birth' 
+            message: 'Please enter a valid date of birth'
         });
     }
-    
+
     else {
         User.findOne({ email })
             .then(user => {
                 if (user) {
-                    return res.status(400).json({ 
+                    return res.status(400).json({
                         status: 'error',
-                        message: 'User already exists' 
+                        message: 'User already exists'
                     });
                 } else {
                     const newUser = new User({
@@ -53,45 +55,114 @@ router.post('/signup', (req, res) => {
                         password,
                         dateofbirth
                     });
-                    // Create salt & hash
-                    bcrypt.genSalt(10, (err, salt) => {
-                        bcrypt.hash(newUser.password, salt, (err, hash) => {
-                            if (err) throw err;
-                            newUser.password = hash;
-                            newUser.save()
-                                .then(user => {
-                                    jwt.sign(
-                                        { id: user.id },
-                                        process.env.jwtSecret,
-                                        { expiresIn: 3600 },
-                                        (err, token) => {
-                                            if (err) throw err;
-                                            res.json({
-                                                token,
-                                                user: {
-                                                    id: user.id,
-                                                    name: user.name,
-                                                    email: user.email
-                                                }
-                                            });
-                                        }
-                                    );
-                                });
+
+                    const saltRounds = 10;
+                    bcrypt.hash(password, saltRounds).then(hashedPassword => {
+                        const newUser = new User({
+                            name,
+                            email,
+                            password: hashedPassword,
+                            dateofbirth
                         });
+                        newUser.save()
+                            .then(user => {
+                                res.json({
+                                    status: 'success',
+                                    message: 'User created successfully',
+                                    data: user
+                                });
+                            })
+                            .catch(err => {
+                                res.json({
+                                    status: 'error',
+                                    message: 'Error creating user',
+                                    data: err
+                                });
+                            })
+
+                            .catch(err => {
+                                res.status(500).json({
+                                    status: 'error',
+                                    message: 'An error occurred hashing the password',
+                                    data: err
+                                });
+                            });
                     });
                 }
-            }).catch(err => {
-                res.status(400).json({ 
+            })
+            .catch(err => {
+                res.status(500).json({
                     status: 'error',
-                    message: 'Error occured While checking if user exists' 
+                    message: 'An error occurred while checking for existing user',
+                    data: err
+
                 });
             });
     }
 });
 
+
 //login 
 router.post('/login', (req, res) => {
-    res.send('login');
+    let { email, password } = req.body;
+    email = email.trim();
+    password = password.trim();
+    if (!email || !password) {
+        return res.status(400).json({
+            status: 'error',
+            message: 'Please enter all fields'
+        });
+    } else if (!/^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/.test(email)) {
+        res.json({
+            status: 'error',
+            message: 'Please enter a valid email'
+        });
+    } else if (password.length < 8) {
+        return res.status(400).json({
+            status: 'error',
+            message: 'Password must be at least 6 characters'
+        });
+    } else {
+        User.findOne({ email })
+            .then(user => {
+                if (!user) {
+                    return res.status(400).json({
+                        status: 'error',
+                        message: 'User does not exist'
+                    });
+                } else {
+                    bcrypt.compare(password, user.password)
+                        .then(isMatch => {
+                            if (!isMatch) {
+                                return res.status(400).json({
+                                    status: 'error',
+                                    message: 'Invalid password'
+                                });
+                            } else {
+                                res.json({
+                                    status: 'success',
+                                    message: 'Login successful',
+                                    data: user
+                                });
+                            }
+                        })
+                        .catch(err => {
+                            res.status(500).json({
+                                status: 'error',
+                                message: 'An error occurred while comparing passwords',
+                                data: err
+                            });
+                        });
+                }
+            })
+            .catch(err => {
+                res.status(500).json({
+                    status: 'error',
+                    message: 'An error occurred while checking for existing user',
+                    data: err
+                });
+            });
+    }
 });
 
 module.exports = router;
--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 require('./config/db');
 
@@ -6,10 +7,11 @@ const port = process.env.PORT || 3000;
 const userRouter = require('./api/User');
 
 //middleware
-// app.use(require('body-parser').json());
-// express.urlencoded([options])
+//app.use(require('body-parser').json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/", userRouter);
+app.use(express.json());
+app.use("/user", userRouter);
+app.use(morgan('dev'));
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
